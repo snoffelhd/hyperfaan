d02aaa1906fd982420161c8e5d64571ca4706047
snoffelhd <32904082+snoffelhd@users.noreply.github.com>
2017-10-18 16:52:35 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (message.content === 'ping') {
-    	message.reply('lol');
+    	message.reply('halo i bims. lol');
   	}
 });
 
