37b64996a573ac830610ad4d1cb1c1c4749e0d94
snoffelhd <32904082+snoffelhd@users.noreply.github.com>
2017-10-18 17:41:06 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,9 +6,16 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
+  if (message.content === '!play') {
+    // Note that this will only work if the message was sent in a guild
+    // and the author is actually in a voice channel.
+    // You might want to check for all that stuff first
+    const channel = message.member.voiceChannel;
+
+    channel.join()
+    .then(connection => console.log('Connected!'))
+    .catch(console.error);
+  }
 });
 
 // THIS  MUST  BE  THIS  WAY
