314495585e1ae84676702846a8938100c166e231
snoffelhd <32904082+snoffelhd@users.noreply.github.com>
2017-10-18 17:01:26 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,19 +1,15 @@
 const Discord = require('discord.js');
-
 const client = new Discord.Client();
 
-// Joins the voice channel as soon as the client starts up
 client.on('ready', () => {
-  // Get the channel via ID
-  let channel = client.channels.get('368449033003597834');
-
-  channel.join()
-  .then(connection => console.log('Connected'))
-  .catch(console.error);
+    console.log('I am ready!');
 });
 
 client.on('message', message => {
-  //...
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
 });
 
-client.login('BOT_TOKEN');
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
